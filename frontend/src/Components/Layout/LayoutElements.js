da1c523999f01885e0a253c5ed5e2051da7d039c
Gonzalo Barrios <gonza.barrios.geb@gmail.com>
2022-12-06 11:12:56 -0300

    init

---
+++ frontend/src/Components/Layout/LayoutElements.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background: linear-gradient(to left bottom, #0a192f, #0f1943);
+`;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Layout from "../Components/Layout";
+/*
+import HeroSection from "../components/HeroSection/Index";
+import InfoSection from "../components/InfoSection";
+import { about } from "../components/InfoSection/Data";
+import Services from "../components/Services";
+import Skills from "../components/Skills";
+import Footer from "../components/Footer";
+import Works from "../components/Works";
+import Header from "../components/Header";
+import Left from "../components/Left";
+import Right from "../components/Right";
+import Content from "../components/Content";
+import Contact from "../components/Contact";
+import Main from "../components/Main";
+
+import Slider from "../components/Caroussel/Slider";*/
+
+const Home = () => {
+  return (
+    <Layout>
+      {/* <Left />
+      <Right />
+
+      <Content>
+        <Header />
+        <Main>
+          <HeroSection />
+          <InfoSection {...about} />
+          <Skills />
+          <Works />
+          <Services />
+          <Contact />
+          <Slider />
+        </Main>
+
+        <Footer />
+      </Content>
+
+  */}
+    </Layout>
+  );
+};
+
+export default Home;
